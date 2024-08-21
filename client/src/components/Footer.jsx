6889f2374101c084b6d24424e98133da793f0152
janna <jannakondeth5@gmail.com>
2024-08-21 11:17:39 +0530

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import logo from '../assets/logo1.png'
+import facebook from '../assets/facebook 1.png'
+import twitter from '../assets/twitter 1.png'
+import linkedin from '../assets/linkedin 1.png'
+import youtube from '../assets/youtube 1.png'
+import call from '../assets/callicon.png'
+
+function Footer() {
+  return (
+    <div className='min-h-[350px]'>
+      <div className='pt-[34px]'>
+        <img src={logo} alt='logo'  className='w-[134px] h-[48px] ml-[180px] '/>
+      </div>
+
+      <div className='flex mt-[50px] gap-[100px]   '>
+
+
+         <div className='ml-[180px]'>
+           <p className='font_poppins text-sm font-semibold leading-3 ' style={{ letterSpacing: '0.75px' }}>CONNECTED WITH US</p>
+           <div className='flex  mt-[20px] gap-[15px]'>
+           <div  className='bg-[#E9E9E9] h-[40px] w-[40px] rounded-full flex justify-center items-center '>
+              <img src={facebook} alt='fbicon'  className='w-[20px]' />
+           </div>
+           <div  className='bg-[#E9E9E9] h-[40px] w-[40px] rounded-full flex justify-center items-center '>
+              <img src={twitter} alt='twittericon'  className='w-[20px]' />
+           </div>
+           <div  className='bg-[#E9E9E9] h-[40px] w-[40px] rounded-full flex justify-center items-center '>
+              <img src={linkedin} alt='linkedinicon'  className='w-[20px]' />
+           </div>
+           <div  className='bg-[#E9E9E9] h-[40px] w-[40px] rounded-full flex justify-center items-center '>
+              <img src={youtube} alt='yticon'  className='w-[20px]' />
+           </div>
+           </div>
+         </div>
+
+
+         <div>
+         <p className='font_poppins text-sm font-semibold leading-3 ' style={{ letterSpacing: '0.75px' }}>IMPORTANT LINKS</p>
+         <div className='flex gap-[15px] mt-[30px]'>
+           <p className='font_poppins font-normal text-xs leading-[26px] text-[#777777]'>Terms & Conditions</p>
+           <p  className='font_poppins font-normal text-xs leading-[26px] text-[#777777]'>Privacy Policy</p>
+           <p  className='font_poppins font-normal text-xs leading-[26px] text-[#777777]'>Help & FAQs</p>
+         </div>
+         </div>
+
+
+         <div className='flex'>
+          
+         <div className="w-[1px] h-[120px] opacity-40 bg-[#D9D9D9] mt-[-20px]">
+                    {/* Vertical line */}
+          </div>
+          <div className='ml-[100px] space-y-[10px] '>
+                  <div  className='bg-[#1AA5C3] h-[40px] w-[40px] rounded-full flex justify-center items-center '>
+                      <img src={call} alt='callicon'  className='w-[20px]' />
+                  </div>
+                  <p className='font_poppins font-normal text-[15px] leading-[20px] text-[#1B1B1B]' >Helpline</p>
+                  <h1 className='font_poppins font-semibold text-[26px] leading-[20px] text-[#1B1B1B]'  style={{ letterSpacing: '0.35px' }} > 1800 456 84788</h1>
+          </div>
+         </div>
+      </div>
+
+
+     <div className='w-[1100px] h-[1px] bg-[#DCDCDC] mt-[50px] mx-auto'>
+      {/* horizontal line */}
+     </div>
+
+     <div className=' flex justify-center items-center mt-[20px]'>
+      <p className='font_poppins font-normal leading-[13px] text-sm text-[#777777]' >Arab Deals  © 2023. All Rights Reserved</p>
+     </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <div className='min-h-[600px] bg-[#F4F4F4] flex justify-center items-center '>
+       <h1 className='text-[#222529]'>Login/Register to your account</h1>
+    </div>
+  )
+}
+
+export default LoginForm
