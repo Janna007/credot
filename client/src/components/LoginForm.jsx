bf3097592f99040c4801dda50d2a190765e1faa1
janna <jannakondeth5@gmail.com>
2024-08-24 12:54:03 +0530

    register and login

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import api from '../api/api';
+import { useDispatch } from 'react-redux';
+import {login } from '../store/authSlice.js'
+
 
 function LoginForm() {
   const navigate=useNavigate()
+
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const [userData,setUserData]=useState(null)
+
+  // Handle input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+
+      const response=await api.post("/users/login",formData)
+      console.log(response)
+      
+      const { user, accessToken } = response.data.data;
+
+      if(response.data.statusCode===200){
+        setUserData(user)
+        dispatch(login(userData))
+        localStorage.setItem('token', accessToken);
+        navigate("/home")
+      }
+
+        setError("")
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error.response.data.message)
+    }
+  };
   
   return (
     <div className='min-h-[600px] bg-[#F4F4F4] flex justify-center items-center flex-col  overflow-hidden p-[20px]'>
@@ -10,14 +59,37 @@ function LoginForm() {
 
        <p className='font_poppins text-[#777777] font-normal md:text-[13px] text-[10px] mnd:leading-[13px]  mt-[30px] text-center'>orem ipsum dolor sit amet consectetur. Sapien ut libero sed lacinia egestas placerat ut sagittionec</p>
 
+       <form onSubmit={handleSubmit}>
+
        <div className='flex flex-col gap-[10px] mt-[20px] justify-center items-center'>
-        <input className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white focus:ring-0 focus:border-white p-[5px] '  type='email' placeholder='Email ID'  />
-        <input  className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white p-[5px]'  type='password' placeholder='Password' />
+        <input className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white focus:ring-0 focus:border-white p-[5px]' 
+          type='email' 
+          name='email'
+          placeholder='Email ID' 
+          value={formData.email}
+          onChange={handleChange}
+          required  />
+        <input  className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white p-[5px]' 
+          type='password' 
+          name='password'
+          placeholder='Password' 
+          value={formData.password}
+          onChange={handleChange}
+          required/>
        </div>
 
-       <div className='mt-[30px]'>
-        <button className='bg-[#1AA5C3]  w-[176px] h-[52px] text-white'>Login</button>
+
+       {error && (
+        <p className="mt-2 p-3 text-red-600  text-center font_poppins">
+          {error}
+        </p>
+      )}
+
+       <div className='mt-[30px] flex justify-center items-center'>
+        <button  type='submit' className='bg-[#1AA5C3]  w-[176px] h-[52px] text-white'>Login</button>
        </div>
+
+       </form>
 
        <p className='mt-[20px] '>Dont have account ?<span className='font-semibold' onClick={()=>{navigate("/register")}}>Register</span> </p>
     </div>
--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import api from '../api/api.js'
+
 
 function RegisterForm() {
   const navigate=useNavigate()
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+
+  const [error,setError]=useState(null)
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response=await api.post("/users/register",formData)
+     console.log(response)
+
+     if(response.data.statusCode===200){
+      navigate("/")
+     }
+
+     setError(" ")
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error.response.data.message)
+    }
+  };
+
   
   return (
     <div className='min-h-[600px] bg-[#F4F4F4] flex justify-center items-center flex-col  overflow-hidden p-[20px]'>
@@ -10,14 +47,30 @@ function RegisterForm() {
 
        <p className='font_poppins text-[#777777] font-normal md:text-[13px] text-[10px] mnd:leading-[13px]  mt-[30px] text-center'>orem ipsum dolor sit amet consectetur. Sapien ut libero sed lacinia egestas placerat ut sagittionec</p>
 
+       <form onSubmit={handleSubmit}>
+
        <div className='flex flex-col gap-[10px] mt-[20px] justify-center items-center'>
-        <input className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white focus:ring-0 focus:border-white p-[5px] '  type='email' placeholder='Email ID'  />
-        <input  className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white p-[5px]'  type='password' placeholder='Password' />
+        <input  className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white focus:ring-0 focus:border-white p-[5px]' 
+          type='email' 
+          name='email'
+          placeholder='Email ID' 
+          value={formData.email}
+          onChange={handleChange} />
+        <input  className='md:w-[538px] w-[300px] h-[60px] border border-1 border-[#CDCDCD] bg-white p-[5px]' 
+          type='password' 
+          name='password'
+          placeholder='Password' 
+          value={formData.password}
+          onChange={handleChange} />
        </div>
 
-       <div className='mt-[30px]'>
-        <button className='bg-[#1AA5C3]  w-[176px] h-[52px] text-white'>Create Account</button>
+       <div className='mt-[30px] flex justify-center items-center'>
+        <button type='submit'  className='bg-[#1AA5C3]  w-[176px] h-[52px] text-white'>Create Account</button>
        </div>
+
+       </form>
+
+       <p className='mt-2 p-3 text-red-600 rounded-md text-center font_poppins'>{error}</p>
 
        <p className='mt-[20px] '>Already have account ?<span className='font-semibold' onClick={()=>{navigate("/")}}>Login</span> </p>
     </div>
