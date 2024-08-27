6ac937eb6d40dae8ef299e2cb985a116ee5b8323
janna <jannakondeth5@gmail.com>
2024-08-27 13:56:54 +0530

    bug

--- client/src/components/home/ProductPhone.jsx
+++ client/src/components/home/ProductPhone.jsx
@@ -106,29 +106,30 @@ function ProductPhone() {
      {/* //cards */}
    
 
-     <div className="flex flex-wrap justify-center mt-[30px] gap-4 px-4 md:px-6 lg:px-8">
-  {products.map((product) => (
-    <Link to={`/product/${product._id}`} key={product._id} className='w-full sm:w-[250px] md:w-[300px] lg:w-[380px]'>
-      <div className='bg-white p-4 h-full sm:h-[400px] md:h-[450px] lg:h-[500px] border-[#B9B9B9] border-[1px] flex flex-col justify-center items-center'>
-        <div className='relative flex flex-col items-center'>
-        <img src={hot} alt='hot' className='absolute top-[-10px] left-[-4px]' />
-          <img src={phone} className='h-[100px] md:h-[150px] lg:h-[200px] w-auto' alt='product' />
-          <img src={add} alt='add' className='absolute right-0' />
-          <div className='flex flex-col gap-[10px] mt-[20px] md:mt-[30px] text-center'>
-            <p className='font_PlusJakartaSans text-[#1AA5C3] font-bold text-[10px] md:text-[12px] lg:text-[14px] leading-[15px] md:leading-[18px] uppercase' style={{ letterSpacing: "1px" }}>{product.category}</p>
-            <h1 className='text-[#000000] font_poppins font-medium text-[14px] md:text-[16px] lg:text-[18px] leading-[20px] md:leading-[23px]'>{product.name}</h1>
-            <div className='flex flex-col md:flex-row gap-[10px] md:gap-[15px] items-center'>
-              <p className='text-[#606060] font-bold text-[12px] md:text-[14px] lg:text-[16px] leading-[15px]'>INR</p>
-              <p className='text-[#000000] font-extrabold text-[18px] md:text-[20px] lg:text-[22px] leading-[15px]'>{product.price}</p>
-              <p className='text-[#777777] font-semibold text-[14px] md:text-[16px] lg:text-[18px] leading-[15px] line-through'>5000.00</p>
-            </div>
-          </div>
-        </div>
+     <div className="flex flex-wrap justify-center mt-[30px] gap-4 px-[30px] md:px-[50px] lg:px-[110px]">
+   
+   {products.map((product) => (
+       <Link to={`/product/${product._id}`}>
+     <div key={product._id} className='bg-white p-4 w-full sm:w-[250px] md:w-[300px] lg:w-[380px] h-[350px] sm:h-[400px] md:h-[450px] lg:h-[500px] border-[#B9B9B9] border-[1px] flex justify-center items-center flex-col'>
+       <div className='flex justify-center items-center flex-col relative'>
+         <img src={hot} alt='hot' className='absolute top-[-10px] left-[-4px]' />
+         <img src={phone} className='h-[100px] md:h-[150px] lg:h-[200px] w-auto' />
+         <img src={add} alt='add' className='absolute right-0' />
+         <div className='flex flex-col gap-[10px] mt-[20px] md:mt-[30px]'>
+           <p className='font_PlusJakartaSans text-[#1AA5C3] font-bold text-[10px] md:text-[12px] lg:text-[14px] leading-[15px] md:leading-[18px] uppercase' style={{ letterSpacing: "1px" }}>{product.category}</p>
+           <h1 className='text-[#000000] font_poppins font-medium text-[14px] md:text-[16px] lg:text-[18px] leading-[20px] md:leading-[23px]'>{product.name}</h1>
+           <div className='flex gap-[10px] md:gap-[15px]'>
+             <p className='text-[#606060] font-bold text-[12px] md:text-[14px] lg:text-[16px] leading-[15px]'>INR</p>
+             <p className='text-[#000000] font-extrabold text-[18px] md:text-[20px] lg:text-[22px] leading-[15px]'>{product.price}</p>
+             <p className='text-[#777777] font-semibold text-[14px] md:text-[16px] lg:text-[18px] leading-[15px] line-through'>5000.00</p>
+           </div>
+         </div>
+       </div>
+     </div>
+     </Link>
+   ))}
+  
       </div>
-    </Link>
-  ))}
-    </div>
-
 
 
 
